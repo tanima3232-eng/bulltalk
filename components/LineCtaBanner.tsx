0811154b728a42c9ba3fc0762fc3c57dec9447c0
tanima3232-eng <tanima3232@gmail.com>
2026-04-01 18:54:53 +0900

    fix: LINE CTAボタン「LINEで無料相談する」に変更

--- components/LineCtaBanner.tsx
+++ components/LineCtaBanner.tsx
@@ -14,7 +14,7 @@ export default function LineCtaBanner() {
           <svg className="w-6 h-6" viewBox="0 0 24 24" fill="currentColor">
             <path d="M12 2C6.48 2 2 6.04 2 11c0 3.45 1.92 6.47 4.8 8.19L6 22l2.72-1.44C9.77 20.83 10.87 21 12 21c5.52 0 10-4.04 10-9S17.52 2 12 2z"/>
           </svg>
-          LINE で無料登録する
+          LINEで無料相談する
         </a>
 
       </div>
